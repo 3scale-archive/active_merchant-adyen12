910b3cc45e4aee9f12eb905736508a593ef1416f
Hallelujah <hery@rails-royce.org>
2017-11-01 12:59:09 +0100

    [test] Make test work using ActiveMerchant helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,4 +1,18 @@
+require 'bundler'
+
+Bundler.require :development, :test
+
+spec = Gem::Specification.find_by_name('activemerchant')
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+$LOAD_PATH.unshift File.join(spec.gem_dir, 'test')
 
 require 'active_merchant/adyen12'
+
+module ActiveMerchant
+  module Fixtures
+    DEFAULT_CREDENTIALS = File.join(File.dirname(__FILE__), 'fixtures.yml') unless defined?(DEFAULT_CREDENTIALS)
+  end
+end
+
+require File.join(spec.gem_dir, 'test', 'test_helper.rb')
